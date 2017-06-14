52ebaf58e8bf1f13de3b9ed61d18911369c471a3
Monte Jiran <montejiran@gmail.com>
2017-06-14 09:57:34 -0700

    complete can_string_be_palindrome?

--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,5 +1,18 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  chars_hash = HashMap.new
+  string.chars.each do |char|
+    if chars_hash[char]
+      chars_hash[char] += 1
+    else
+      chars_hash[char] = 1
+    end
+  end
+
+  num_odd = 0
+  chars_hash.each do |key, val|
+    num_odd += 1 unless val % 2 == 0
+  end
+  return num_odd < 2
 end
