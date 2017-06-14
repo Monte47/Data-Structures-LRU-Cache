b6f3dc0bb12fc56d60983db4db227a14384e5ee8
Monte Jiran <montejiran@gmail.com>
2017-06-13 20:41:51 -0700

    complete hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    sum = 0
+    flatten.each_with_index do |el, i|
+      sum += (el * i)
+    end
+    sum.hash
   end
 end
 
 class String
   def hash
+    chars.map { |char| char.ord.hash }.hash
   end
 end
 
@@ -16,6 +22,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    each do |k, v|
+      sum += (k.to_s + v.to_s).hash
+    end
+    sum.hash
   end
 end
