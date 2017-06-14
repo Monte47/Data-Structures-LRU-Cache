ab83bed2a43ef4ea0cb70ae6741dc4e54eef9b5f
Monte Jiran <montejiran@gmail.com>
2017-06-13 21:13:37 -0700

    complete linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -19,7 +19,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new(:head)
+    @tail = Node.new(:tail)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +34,63 @@ class LinkedList
   end
 
   def first
+    @head.next unless empty?
   end
 
   def last
+    @tail.prev unless empty?
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each do |link|
+      return link.val if link.key == key
+    end
   end
 
   def include?(key)
+    each do |link|
+      return true if link.key == key
+    end
+    false
   end
 
   def append(key, val)
+    new_link = Node.new(key, val)
+    new_link.prev = @tail.prev
+    new_link.next = @tail
+    @tail.prev.next = new_link
+    @tail.prev = new_link
   end
 
   def update(key, val)
+    each do |link|
+      link.val = val if link.key == key
+    end
   end
 
   def remove(key)
+    each do |link|
+      if link.key == key
+        link.prev.next = link.next
+        link.next.prev = link.prev
+      end
+    end
   end
 
   def each
+    start = @head.next
+    until start == @tail
+      yield start
+      start = start.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
 end
